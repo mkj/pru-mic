993426bb6bc9810e7e643cf4e1d0e1e65474ef0f
Matt Johnston <matt@ucc.asn.au>
2016-07-02 07:49:09 +0800

    fixes for cycles

--- waitcycle.asm
+++ waitcycle.asm
@@ -9,8 +9,8 @@
 ||newcycle||
         LDI32     r0, 0x0002200c
         LDI       r1, ||cyclestart||
-        LBBO      &r0, r0, 0, 4
-        SUB       r0, r0, 0x02
+        LBBO      &r0, r0, 0, 4   ; load CYCLE register - extra delay from here onwards
+        ADD       r0, r0, 4 ; cycle register 4 cycles later when this function returns
         SBBO      &r0, r1, 0, 4
         JMP       r3.w2
 
@@ -25,15 +25,16 @@
 	.global	||waitcycle||
 ||waitcycle||
         LDI32     r0, 0x0002200c
-        LBBO      &r1, r0, 0, 4   ; load CYCLE register
+        LBBO      &r1, r0, 0, 4   ; load CYCLE register - extra delay from here
         LDI       r0, ||cyclestart||
         LBBO      &r0, r0, 0, 4   ; load cyclestart
         ADD       r0, r0, r14
-        RSB       r0, r1, r0      ; delay = (cyclestart + until) - now;
-        SUB       r0, r0, 8       ; subtract cycles used in this function
-        QBBC      ||$delaycmp||, r0, 1    ; odd delay gets a single subtraction
-||$delaysub||:
-        SUB       r0, r0, 0x01    ; delay--
-||$delaycmp||:
-        QBNE      ||$delaysub||, r0, 0x00
+        RSB       r0, r1, r0      ; delay = (cyclestart + until) - now
+        SUB       r0, r0, 10       ; subtract extra delay=10
+        LSR       r15, r0, 1      ; divide delay by two, delayloop is 2 insns SUB/QBNE
+        QBBC      ||$delayloop||, r0, 1
+        MOV       r0, r0         ; nop for odd delay extra instruction
+||$delayloop||:
+        SUB       r15, r15, 0x01    ; delay--
+        QBNE      ||$delayloop||, r15, 0x00
         JMP       r3.w2
