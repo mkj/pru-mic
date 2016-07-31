0b823ba8a63277a5ae240ea60274f0ee832876fc
Matt Johnston <matt@ucc.asn.au>
2016-07-31 23:11:29 +0800

    fiddling with pdm and demux

--- pdm/pdm1.py
+++ pdm/pdm1.py
@@ -3,34 +3,116 @@
 import struct
 import sys
 import itertools
+import wave
+import io
+
+import scipy.signal
 import numpy as np
 import numba
+import matplotlib.pyplot as plt
+
+import wiggle
+
 
 NSTREAMS = 8
+
+TESTMIC = 2 # data2
+
+F = 1e6
+
 
 np.set_printoptions(threshold=np.inf)
 
 def demux(b):
 	""" demultiplexes b """
 	b.shape = (1,b.shape[0])
-	return np.unpackbits(b, axis=0)
+	bits = np.unpackbits(b, axis=0)
+	return np.array(bits, dtype=np.int32)
 
 def demuxstream(f, chunk):
 	""" yields arrays of bits """
 	while True:
-		r = np.fromfile(f, count=chunk, dtype=np.uint8, )
+		r = np.fromfile(f, count=chunk, dtype=np.uint8)
 		if r.size != chunk:
 			return
 		yield demux(r)
 
-class decoder(object):
+class sumdecoder(object):
 	def __call__(self, s):
 		return np.sum(s)
 
+class decimater(object):
+	def __init__(self, decim=128):
+		self.decim = decim
+	def __call__(self, s):
+		newl = s.shape[0] / self.decim
+		#return scipy.signal.resample(s, newl)
+		k = s
+		k = scipy.signal.decimate(k, 8, 30, 'fir', zero_phase=True)
+		#k = scipy.signal.decimate(k, 8, 50, 'fir', zero_phase=True)
+		#k = scipy.signal.decimate(k, 4, 50, 'fir', zero_phase=True)
+		#k = scipy.signal.decimate(k, 4, 50, 'fir', zero_phase=True)
+		return k
+
 def decodestream(f, chunk):
-	decoders = [decoder() for _ in range(NSTREAMS)]
+	decoders = [decimater() for _ in range(NSTREAMS)]
 	for a in demuxstream(sys.stdin, chunk):
 		yield [decode(x) for (decode, x) in zip(decoders, a)]
 
-for n in decodestream(sys.stdin, 64):
-	print(n)
+def decodeone(f, chunk, decim):
+	decoder = decimater(decim)
+	for a in demuxstream(sys.stdin, chunk):
+		yield decoder(a[NSTREAMS-1-TESTMIC])
+
+def demuxone(f, chunk):
+	for a in demuxstream(sys.stdin, chunk):
+		yield a[NSTREAMS-1-TESTMIC]
+
+#for n in decodeone(sys.stdin, 128000):
+#	print(n)
+
+def writewav(fn, sampbuf, rate):
+	with wave.open(fn, 'wb') as w:
+		w.setnchannels(1)
+		w.setsampwidth(2)
+		w.setframerate(rate)
+
+		w.writeframes(sampbuf)
+
+decim = 128
+
+insamps = next(demuxone(sys.stdin, 128000)) * 2 - 1
+print("insamps mean %f rms %f" % (np.mean(insamps), np.mean(insamps**2)**0.5))
+print("insamps min %f max %f" % (np.min(insamps), np.max(insamps)))
+
+print(insamps[:200])
+insamps.tofile('i1.dat', '')
+
+decoder = decimater(decim)
+samps = decoder(insamps)
+
+rate = F/decim
+print("sample rate %s" % rate)
+
+#ls = np.sign(samps) * np.log(np.abs(samps))
+ls = samps
+#print(ls)
+
+rms = np.sqrt(np.mean(ls**2))
+scale = 32768 / rms
+mean = np.mean(ls)
+
+print("rms %f, scale %f, mean %f" % (rms, scale, mean))
+
+#ls = ls * scale
+
+wavbuf = ls.astype('int16').tobytes()
+
+with open('test.dat', 'wb') as f:
+	f.write(wavbuf)
+
+writewav('out1.wav', wavbuf, rate)
+
+fig = plt.figure(1)
+plt.plot(ls)
+plt.show()
---
+++ pdm/wiggle.py
@@ -0,0 +1,61 @@
+"""
+Copyright (c) 2013 Stewart Fletcher
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+import numpy as np
+import pylab
+
+def wiggle(frame, scale=1.0):
+        fig = pylab.figure()
+        ax = fig.add_subplot(111)        
+        ns = frame['ns'][0]
+        nt = frame.size
+        scalar = scale*frame.size/(frame.size*0.2) #scales the trace amplitudes relative to the number of traces
+        frame['trace'][:,-1] = np.nan #set the very last value to nan. this is a lazy way to prevent wrapping
+        vals = frame['trace'].ravel() #flat view of the 2d array.
+        vect = np.arange(vals.size).astype(np.float) #flat index array, for correctly locating zero crossings in the flat view
+        crossing = np.where(np.diff(np.signbit(vals)))[0] #index before zero crossing
+        #use linear interpolation to find the zero crossing, i.e. y = mx + c. 
+        x1=  vals[crossing]
+        x2 =  vals[crossing+1]
+        y1 = vect[crossing]
+        y2 = vect[crossing+1]
+        m = (y2 - y1)/(x2-x1)
+        c = y1 - m*x1       
+        #tack these values onto the end of the existing data
+        x = np.hstack([vals, np.zeros_like(c)])
+        y = np.hstack([vect, c])
+        #resort the data
+        order = np.argsort(y) 
+        #shift from amplitudes to plotting coordinates
+        x_shift, y = y[order].__divmod__(ns)
+        ax.plot(x[order] *scalar + x_shift + 1, y, 'k')
+        x[x<0] = np.nan
+        x = x[order] *scalar + x_shift + 1
+        ax.fill(x,y, 'k', aa=True) 
+        ax.set_xlim([0,nt])
+        ax.set_ylim([ns,0])
+        pylab.tight_layout()
+        pylab.show()
+        
+
+
+    
+    
