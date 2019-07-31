4a834e445aebd81eb0cc7e6b61f15c0308d2273a
Matt Johnston <matt@ucc.asn.au>
2019-07-31 19:52:21 +0800

    linger for socat

--- bitex/listenall.sh
+++ bitex/listenall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for i in `seq 0 7`; do
-    socat TCP-LISTEN:999$i,fork EXEC:"./bitex $i /dev/bulk_samp31" &
+    socat TCP-LISTEN:999$i,fork,linger=2 EXEC:"./bitex $i /dev/bulk_samp31" &
 done
 
 wait
