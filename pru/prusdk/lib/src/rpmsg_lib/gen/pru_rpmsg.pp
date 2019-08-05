519ee96f410270ee2b0bd3763fa3f4849527a0b1
Matt Johnston <matt@ucc.asn.au>
2019-08-05 23:30:22 +0800

    remove old PRU sdk copy

--- pru/prusdk/lib/src/rpmsg_lib/gen/pru_rpmsg.pp
+++
@@ -1,7 +0,0 @@
-gen/pru_rpmsg.object: pru_rpmsg.c
-gen/pru_rpmsg.object: ../../../include/pru_rpmsg.h
-gen/pru_rpmsg.object: ../../../include/pru_virtqueue.h
-gen/pru_rpmsg.object: ../../../include/rsc_types.h
-gen/pru_rpmsg.object: /home/a0273976local/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.1//include/stdint.h
-gen/pru_rpmsg.object: ../../../include/pru_types.h
-gen/pru_rpmsg.object: ../../../include/pru_virtio_ring.h
