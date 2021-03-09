7fb4f37017950b4919463a1e5ae06350b593905d
Matt Johnston <matt@ucc.asn.au>
2021-03-09 23:09:53 +0800

    first rust CIC works

---
+++ live/src/bulksamp.rs
@@ -0,0 +1,58 @@
+#[allow(unused_imports)]
+use {
+    log::{debug, error, info, warn},
+    anyhow::{Result,Context,bail,anyhow},
+};
+use std::io::{Read,BufReader};
+
+// TODO: const generics
+// Samples per channel per chunk. bytes of input, bits per channel of output
+const CHUNK: usize = 64;
+pub type Sample = i32;
+
+pub struct BulkSamp {
+    inf: std::io::BufReader<std::fs::File>,
+}
+
+impl BulkSamp {
+    pub fn new(infile: &str) -> Result<Self> {
+
+        Ok(BulkSamp {
+            // TODO: perhaps with_capacity() for lower latency?
+            // Default 8kB is 2ms.
+            inf: BufReader::new(std::fs::File::open(infile)?),
+        })
+    }
+}
+
+impl Iterator for BulkSamp {
+    type Item = Result<[[Sample; CHUNK]; 8]>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut buf = [0u8; CHUNK];
+        match self.inf.read(&mut buf) {
+            Ok(nread) => {
+                if nread < CHUNK {
+                    // discard the last chunk if it's not a full multiple!
+                    return None
+                }
+            }
+            Err(e) => {
+                return Some(Err(e).context("Error reading input"))
+            }
+        };
+
+        // demultiplex
+        let mut res = [[0 as Sample; CHUNK]; 8];
+        for i in 0..CHUNK {
+            for c in 0..8 {
+                res[c][i] = ((((buf[i] >> c) & 1) as i32) << 16) - 32768;
+            }
+        }
+        Some(Ok(res))
+    }
+}
+
+
+
+
---
+++ live/src/main.rs
@@ -0,0 +1,99 @@
+#[allow(unused_imports)]
+use {
+    log::{debug, error, info, warn},
+    anyhow::{Result,Context,bail,anyhow},
+};
+use simplelog::{CombinedLogger,LevelFilter,TermLogger,TerminalMode};
+
+use std::io::BufWriter;
+use std::fs::File;
+
+mod bulksamp;
+
+#[derive(argh::FromArgs)]
+/** AnyPitch
+Matt Johnston 2021 matt@ucc.asn.au */
+struct Args {
+    #[argh(switch, short='v')]
+    /// verbose debug logging
+    debug: bool,
+
+    /// input device, default /dev/bulk_samp31
+    #[argh(option, short = 'd', default = "\"/dev/bulk_samp31\".to_string()")]
+    input: String,
+
+    /// channel 0-7, default 2
+    #[argh(option, short = 'c', default = "2")]
+    channel: usize,
+
+    /// decim - the downsampling rate, default 64
+    #[argh(option, short = 'r', default = "64")]
+    decim: usize,
+
+    /// output wav file (optional)
+    #[argh(option, short = 'w')]
+    outwav: Option<String>,
+
+}
+
+fn setup_log(debug: bool) -> Result<()> {
+    let level = match debug {
+        true => LevelFilter::Debug,
+        false => LevelFilter::Info,
+    };
+    let logconf = simplelog::ConfigBuilder::new()
+    .set_time_format_str("%Y-%m-%d %H:%M:%S%.3f")
+    .set_time_to_local(true)
+    .build();
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(level, logconf.clone(), TerminalMode::Mixed),
+            //WriteLogger::new(level, logconf, open_logfile()?),
+        ]
+    ).context("logging setup failed")
+}
+
+fn open_wav_out(wav_filename: &str) -> Result<hound::WavWriter<BufWriter<File>>> {
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+    hound::WavWriter::create(wav_filename, spec).context("Error opening output wavfile")
+}
+
+fn main() -> Result<()> {
+    let args: Args = argh::from_env();
+    setup_log(args.debug)?;
+
+    let stream = bulksamp::BulkSamp::new(&args.input)?;
+
+    let q = 5; // order
+    let mut cic = sdr::CIC::<bulksamp::Sample>::new(q, args.decim, 1);
+
+    let mut wav = if let Some(f) = args.outwav {
+        match open_wav_out(&f) {
+            Ok(w) => Some(w),
+            // faffing around because hound's Error is odd?
+            // https://github.com/dtolnay/anyhow/issues/35#issuecomment-547986739
+            Err(e) => bail!(e), 
+        }
+    } else {
+        None
+    };
+
+    for s in stream {
+        let c = s?[args.channel];
+        let wout = cic.process(&c);
+        //println!("c {:?}", c);
+        println!("wout {:?}", wout);
+        if let Some(ref mut w) = wav {
+            for s in wout {
+                w.write_sample(s)?;
+            }
+        }
+    }
+
+    Ok(())
+}
